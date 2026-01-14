ee1baecd918a379da9cad2b23a18c80a4658a8dd
jadesand <jadesand@stanford.edu>
2026-01-14 12:17:28 -0800

    initial commit;

---
+++ noise_fast_and_superfast_script_pickle_ba150_L0.sh
@@ -0,0 +1,374 @@
+#!/bin/bash
+#
+# This is the script to take fast and superfast noise data in superconducting, mid-transition and normal in BA2.
+# Most of the content has been taken from an old Bicep2 script for the same purpose, and adapted.
+#
+# SF - 2023-01-21
+# SF, BS - 2023-11-24 added norm noise and modified rows/cols/biases for L4 
+# CZ, RS - 2026-01-02 adapted for pickle ba150 L0
+
+# Usage: noise_superfast_script_pickle_ba150_L0.sh dir configprefix [unlatch_value] [max_cols] [bias_mode]
+#   dir:           output directory name (default: test)
+#   configprefix:  config file prefix (default: config)
+#   max_rows:      total number of rows (default: 41 )
+#   max_cols:      total number of columns (16 or 32, default: 16)
+#   unlatch_value: bias value for unlatching detectors (default: 65535)
+#   unlatch_bias_mode: "all" to bias all cols, "half" to bias only first half cols,
+#                      "manual" to use hardcoded values (default: all)
+
+source $MAS_SCRIPT/mas_library.bash # RS: mostly define some functions
+
+dir=${1:-"test"}
+configprefix=${2:-"config"}
+max_cols=${3:-16}            # Total number of columns: 16 or 32 (default: 16)
+max_rows=${4:-41}            # Total number of rows: 41 (default: 41)
+unlatch_value=${5:-65535}    # Unlatching bias value (default: 65535)
+unlatch_bias_mode=${6:-"all"}  # "all", "half", or "manual" (default: all)
+
+if [ ! -d $MAS_DATA/$dir ]; then
+    mkdir $MAS_DATA/$dir
+fi
+
+####################################################################
+# initial set up, and archiving config and script stuff
+####################################################################
+
+# Don't fiddle with tes bias when using mce_reconfig in 'really_freeze_servo.py'
+mas_param set tes_bias_do_reconfig 0
+mas_param set config_sync 0
+# Build row_deselect args dynamically (max_rows zeros)
+row_deselect_args=$(printf '0 %.0s' $(seq 1 $max_rows))
+mas_param set row_deselect $row_deselect_args
+mce_make_config
+mce_reconfig
+
+# Archive scripts and config files
+cp ~/rshi/noise/noise_fast_and_superfast_pickle_ba150_L0.sh $MAS_DATA/$dir/noise_superfast_script
+cp $MAS_DATA/experiment.cfg $MAS_DATA/$dir/experiment.cfg
+configs=("$MAS_DATA"/"$configprefix"*)
+if [ "${#configs[@]}" -ne 1 ]; then
+    echo "Error: expected exactly one config file in $MAS_DATA"
+    printf 'Found:\n'
+    printf '  %s\n' "${configs[@]}"
+    printf 'Please specify a unique configprefix.\n'
+    exit 1
+fi
+cp "${configs[0]}" "$MAS_DATA/$dir/"
+
+
+
+
+
+####################################################################
+# this section has the details for some one-time set ups
+####################################################################
+
+#change row, col, biases !!!
+
+####################################################################
+# SUPERFAST SET UP--> set up the superfast_acq script parameters
+####################################################################
+
+row_len=62 	    #98 gives 250kHz, 62 gives 400kHz, 120 gives 200kHz
+#for superfast data (raw mode + rectangle mode ) sampling frequency is fs=50e6/(row_len*2)
+#(the extra 2 comes from the fact that we set num_rows=2 -- for script stability?--)
+ccnumrows=41
+ccnumcols=1
+
+datarate=$(( $ccnumrows * $ccnumcols ))
+#nsamp = ccnumrows * ccnumcols * fs * t_int
+#nsamp = 164000000 # integration time t_int ~ 10s
+
+sampleuse=$(((4000000)/($ccnumrows)/($ccnumcols) ))
+# sampleuse=$(((4000)/($ccnumrows)/($ccnumcols) )) # quick check
+sampint=$(printf "%.0f\n" "$sampleuse")
+
+samplenum=`command_reply rb rc1 sample_num`
+sampledly=$(( $row_len-$samplenum ))
+
+#############################################################
+# FB_CONST SQRWAVE SET UP --> set up the fb_const squrewave #
+#############################################################
+
+# para=31              #where rc1/rc2 fb_const are physically mapped per mce_status -g
+card1=3              #where rc1 fb_const is physically mapped per mce_status -g
+card2=4              #where rc2 fb_const is physically mapped per mce_status -g
+period=50            #min is 8000/41=195
+stepsize=200          #10 is good, keep it linear --> changed to 200 to increase S/N
+
+####################################################################
+# FAST SET UP --> set up the fast_acq script parameters and save the commands in a set up script
+####################################################################
+
+fast_datamode=1
+fast_ccnumrows=1
+fast_rcnumrows=1
+fast_datarate=1
+
+# fast_script=$MAS_TEMP/fast.scr
+# rm $fast_script
+fast_script=$MAS_DATA/$dir/fast.scr
+echo "wb rca data_mode "$fast_datamode >> $fast_script
+echo "wb cc num_rows_reported "$fast_ccnumrows >> $fast_script
+echo "wb rca num_rows_reported "$fast_rcnumrows >> $fast_script
+echo "wb cc data_rate "$fast_datarate >> $fast_script
+
+#For fast data we use the operational row_len value, so we don't need to add to the script a cmd to set row_len manually
+#The fs for fast data is fs=50e6/(row_len*num_rows*data_rate), so for num_rows=41, row_len=120 --> fs ~ 10kHz
+
+# get and save in variables the default values set up by mce_reconfig
+
+def_rowlen=`command_reply rb sys row_len`
+def_sampdly=`command_reply rb rca sample_dly`
+def_numrows=`command_reply rb sys num_rows`
+def_rcnumcolsrep=`command_reply rb rca num_cols_reported`
+def_ccnumcolsrep=`command_reply rb cc num_cols_reported`
+def_colindex=`command_reply rb rca readout_col_index`
+def_datarate=`command_reply rb cc data_rate`
+
+
+
+####################################################################
+# START DATA ACQUISITION
+####################################################################
+
+# Unlatching detectors before starting bias scan
+# Build unlatch bias command dynamically based on max_cols and unlatch_bias_mode
+# unlatch_bias_mode="all": all columns get unlatch_value
+# unlatch_bias_mode="half": only first half columns get unlatch_value (rest get 0)
+# unlatch_bias_mode="manual": use hardcoded values below
+if [ "$unlatch_bias_mode" = "manual" ]; then
+    echo "unlatch (manual) for 1s, no tile heater"
+    # Edit the line below to set manual bias values:
+    #             0     1     2     3     4     5     6     7     8     9    10    11    12    13    14    15
+    bias_tess 65535 65535 65535 65535 65535 65535 65535 65535 65535 65535 65535 65535 65535 65535 65535 65535
+else
+    unlatch_bias_args=""
+    for ((i=0; i<max_cols; i++)); do
+        if [ "$unlatch_bias_mode" = "half" ] && [ $i -ge $((max_cols / 2)) ]; then
+            unlatch_bias_args="$unlatch_bias_args 0"
+        else
+            unlatch_bias_args="$unlatch_bias_args $unlatch_value"
+        fi
+    done
+    echo "unlatch ($unlatch_value) for 1s, max_cols=$max_cols, unlatch_bias_mode=$unlatch_bias_mode, no tile heater"
+    bias_tess $unlatch_bias_args
+fi
+sleep 1
+
+# start from high bias and step down, assumes detectors have
+# already been biased into the transition
+
+# for tbias in 6000 4000 3000 2750 2500 2250 2000 1750 1500 500 0
+for tbias in 5000 3000 2500 2000 0 
+# for tbias in 2500
+do
+    echo "tes_bias="$tbias
+    dir=$1'/bias'$tbias'/'
+    mkdir $MAS_DATA'/'$dir
+
+    echo "bias and settle for 30s"
+    bias_tess  $tbias 0 0 0 $tbias 0 0 0 0 0 0 0 0 0 0 0
+
+    sleep 30
+
+    ####################################################################
+    # 400Hz standard noise for all channels at the bias
+    ####################################################################
+
+    echo "taking normal, downsampled, noise for all channels at tes_bias="$tbias
+
+    sleep 1
+    mce_reconfig
+    sleep 1
+
+    mce_run $dir'/all_rcs_datamode10' 6800 s # this corresponds to t= #samples/fs (sec), fs=400 Hz
+    # mce_run $dir'/all_rcs' 68
+
+    sleep 1
+    mce_cmd -qx wb rca data_mode 1
+    sleep 1
+
+    mce_run $dir'/all_rcs_datamode1' 6800 s # this corresponds to t= #samples/fs (sec), fs=400 Hz
+
+    sleep 1
+    mce_reconfig
+    sleep 1
+
+    ####################################################################
+    # define the channels to sample here.
+    # loop slowly over the rows, so the servo_freeze only needs to be
+    # performed once per row.
+    ####################################################################
+
+    for row in 31 32 33 34 36 37 38 39 40
+    # for row in 40
+    do
+        case "$row" in
+            31 ) 
+                coluse=(0);;
+            32 ) 
+                coluse=(0);;
+            33 )
+                coluse=(4);;
+            34 )
+                coluse=(0);;
+            36 )
+                coluse=(0 4);;
+            37 )
+                coluse=(0);;
+            38 )
+                coluse=(0);;
+            39 )
+                coluse=(4);;
+            40 )
+                coluse=(4);;
+        esac
+
+        ####################################################################
+        # ACQUIRE FAST DATA: ~10kHz, closed loop, unfiltered feedback
+        ####################################################################
+
+        echo "taking **kHz data for row"$row
+        mas_param set config_sync 0
+        mce_make_config
+        mce_reconfig
+        #
+        # # this is a standard set of operations, use the fast (10kHz) script,
+        # # but write the new readout_row_index
+        #
+        sleep 1
+        mce_cmd -iqf $fast_script
+        sleep 1
+        mce_cmd -qx wb rca readout_row_index $row
+        #
+        sleep 1
+        fast_filename=$dir'/fast_rc1_row'$row
+        mce_run $fast_filename 204000 1  # this corresponds to t= #samples/fs (sec), fs=10 kHz
+        # mce_run $fast_filename 2040 1
+        #
+        sleep 1
+        mce_reconfig  # get back to normal state to freeze the servo
+
+
+        ####################################################################
+        # freeze the servo on a single row, go open loop, take error data (mode=0)
+        #
+        # from here on start writing mce_cmd writes to a script for all cols
+        # in the row of interest, then after accumulating run the script
+        ####################################################################
+        sleep 1
+
+        python ~/rshi/noise/really_freeze_servo_ba150.py -r $row --o --save
+
+        sleep 2
+        fb_val=(`command_reply rb sq1 fb_const`)
+        echo "fb_val="${fb_val[@]}
+
+        script=$MAS_TEMP/noise_superfast.scr
+        rm $script
+
+        ####################################################################
+        # loop quickly over the cols
+        # accumulate a single mce_cmd script for all cols in the row.
+        # set a few scripting things outside the loop
+        ####################################################################
+        echo "wb rca readout_row_index 0" >> $script               #open-loop, this should always be 0
+        echo "wb rca num_rows_reported 1" >> $script
+
+        echo "looping over columns, generating noise_superfast.scr"
+
+        for col in ${coluse[@]}
+        do
+            echo 'col='$col
+            if [ $col -lt 8 ]; then
+                rc=1
+                card=$card1        # set up for the fb_const sqrwave
+            else
+                rc=2
+                card=$card2        # set up for the fb_const sqrwave
+            fi
+
+            ####################################################################
+            # take super-fast noise timestreams: 400kHz, sampling channel of interest (rectangle + raw mode)
+            ####################################################################
+
+            filename=$MAS_DATA/$dir'/superfast_row'$row'_col'$col
+
+            echo "wb sys row_len "$row_len >> $script
+            echo "wb rca sample_dly "$sampledly >> $script
+            echo "wb sys num_rows 2" >> $script  # num_rows: number of rows to be multiplexed.
+            echo "wb rca num_rows_reported 1" >> $script
+            echo "wb rca num_cols_reported 1" >> $script
+            echo "wb cc num_rows_reported "$ccnumrows >> $script
+            echo "wb cc num_cols_reported "$ccnumcols >> $script
+            echo "wb cc data_rate "$datarate >> $script
+            echo "wb rca readout_col_index "$col >> $script
+
+            echo "sleep 10" >> $script  # mce_cmd sleep <microseconds>
+
+            echo "acq_config "$filename" rc"$rc >> $script # acq_config <filename> <readout_card>, configures a single output file to receive MCE frames
+            echo "acq_go "$sampint >> $script
+
+            # return mce to default state
+            # rca num_rows_reported and cc num_rows_reported were not changed
+            echo "wb sys row_len "$def_rowlen >> $script
+            echo "wb rca sample_dly "$def_sampdly >> $script
+            echo "wb sys num_rows "$def_numrows >> $script
+            echo "wb rca num_cols_reported "$def_rcnumcolsrep >> $script
+            echo "wb cc num_cols_reported "$def_ccnumcolsrep >> $script
+            echo "wb cc data_rate "$def_datarate >> $script
+            echo "wb rca readout_col_index "$def_colindex >> $script
+
+            extn=$MAS_DATA/$dir'/calib_row'$row'_col'$col
+
+            echo "fb_const_calib="${fb_val[@]}
+
+            min=$((${fb_val[$col]}-$stepsize))
+            max=$((${fb_val[$col]}+$stepsize))
+            step=$(($max - $min))
+
+            # set up for fast (10 kHz) acquisition, stay in data_mode 0
+
+            echo "wb cc num_rows_reported "$fast_ccnumrows >> $script
+            echo "wb rca num_rows_reported "$fast_rcnumrows >> $script
+            echo "wb cc data_rate "$fast_datarate >> $script
+
+            # set up fb const square wave
+
+            echo "acq_config "$extn" rc"$rc >> $script
+
+            for ifb_const in {1..200}
+            do
+                n=$(($ifb_const%2))
+                if [ $n -lt 1 ]; then
+                    echo "wb rca fb_const "$min $min $min $min $min $min $min $min>> $script
+                else
+                    echo "wb rca fb_const "$max $max $max $max $max $max $max $max>> $script
+                fi
+                echo "sleep 10" >> $script
+                echo "acq_go "$period >> $script
+            done
+
+            echo "sleep 10" >> $script
+
+            echo "wb cc internal_cmd_mode 0" >> $script  #turn off the sqr wave on fb_const
+            echo "wb sq1 fb_const "${fb_val[@]} >> $script  #return to the servo_freeze values
+
+        ####################################################################
+
+        done
+
+        echo "running noise_superfast.scr"
+
+        # time to execute the script accumulated for the row
+        mce_cmd -iqf $script           # -i means don't exit if some command fails
+
+        echo "done with noise_superfast.scr"
+
+        cp $script $MAS_DATA/$dir"/noise_superfast.scr.row"$row
+
+    done
+done
+
+mce_reconfig
---
+++ really_freeze_servo_ba150.py
@@ -0,0 +1,286 @@
+#!/usr/bin/python
+
+# 0. User specifies a row
+# 1a. Write row_order to contain only that row
+# 1b. Set all rows to const_mode (so it never switches) and set const_val = 0 EXCEPT desired row
+# 4. Set adc offsets for all rows to match that row's values; now the MCE is in closed loop and multiplexing but sitting still
+# 5. clear the PID parameters
+# 6. pause 1 second (?)
+# 7. freeze_servo
+
+# 090619 JPF Added enbl_mux = 0
+# 090624 JPF Removed enbl_mux = 0, added mode to read in a file rather than frames directly
+# 090713 JPF Added feature for BAC configuration
+# 090729 JPF Fixed behavior with nonzero off_bias (NO: we still need to use zeros, since we have zeros applied during SQ2SERVO operation)
+# 091020 JPF Set other SQ1s to their off_biases rather than to zero, add option not to do this
+# 110405 JPF HACKED to make fast SQ2 feedback work with a fast-switching BC
+# 110930 JPF Fixed to make fast SQ2 feedback work with a fast-switching BC; now turns off muxing on SQ1/2
+
+import sys, time, os
+sys.path.append('/home/bicep3/python_tools')
+sys.path.append('/usr/mce/mce_script/python')
+# sys.path.append('/src/mas/swig')
+from glob import *
+from optparse import OptionParser
+from numpy import *
+
+from pymce import MCE as mce
+
+from mce_data import *
+
+# hard-coded constants and flags
+MCE_COLS = 8
+
+# parse command line arguments
+o = OptionParser()
+o.add_option('-n','--n-frames',type='int',default=100,dest='n_frames',
+             help='number of frames to average when determining feedback')
+o.add_option('-r','--row',type='int',default=0,dest='row',
+             help='the row to use for all columns')
+o.add_option('--restore',action='store_true',default=False,dest='restore',
+             help='restart the MCE servo (servo_mode=3 + flx_lp_init)')
+o.add_option('--o','--open',action='store_true',default=False,dest='do_open',
+             help='Freeze with the PID servo inactive (servo_mode=0)')
+o.add_option('--save',action='store_false',default=True,dest='do_direct_acq',
+             help='Save and read back files, rather than reading frames directly')
+o.add_option('--zero_sq1_off',action='store_true',default=False,dest='zero_sq1_off',
+             help='Turn the other SQ1s to zero bias, rather the current off_bias')
+o.add_option('--sq1_nomux',action='store_true',default=False,dest='sq1_nomux',
+             help='Fully disable multiplexing for SQ1')
+opts, args = o.parse_args()
+
+# get an MCE interface object
+m = mce()
+
+# Determine name and number of readout cards
+n_rc = len(m.read('rca', 'data_mode'))
+n_cols = n_rc * MCE_COLS
+n_rows = m.read('cc', 'num_rows_reported', array=False)
+
+print('n_rc = ', n_rc)
+print('n_cols = ', n_cols)
+print('n_rows = ', n_rows)
+
+
+# check whether we have fast SQ2 switching # Commented out for B3
+#use_fastSQ2 = True
+#try:
+#    dummy = m.read('sq2','fb_col0')
+#except MCEError:
+#    use_fastSQ2 = False
+
+time.sleep(1)
+
+# check whether our row is OK
+row = opts.row
+if row not in range(0,n_rows):
+    print 'Error: invalid row number!'
+    sys.exit(10)
+row_order = m.read('ac','row_order')
+if row not in row_order:
+    print 'Error: row not in current row_order!'
+    sys.exit(10)
+
+# SET PID LOOP TO STARTING CONFIGURATION
+m.write('rca','servo_mode',[3]*MCE_COLS)
+time.sleep(0.5)
+m.write('rca', 'flx_lp_init', [1])
+time.sleep(1)
+
+# STOP MCE ON SPECIFIED ROW
+print 'Stopping MUX at row ', row
+# set the SQ1 biases => off for all rows
+# NOTE: we need to do this BEFORE changing row order!
+# first, read the old values for later
+on_bias = m.read('ac','on_bias')
+time.sleep(0.1)
+off_bias = m.read('ac','off_bias')
+time.sleep(0.1)
+if opts.zero_sq1_off:
+    # turn every row to zero bias
+    new_off_bias = [0]*41
+    new_on_bias = [0]*41
+else:
+    # turn every row to its off bias
+    new_off_bias = off_bias
+    new_on_bias = off_bias
+m.write('ac','on_bias',new_on_bias)
+m.write('row','select',new_on_bias) #changed from sq1, for B3
+time.sleep(0.2)
+m.write('ac','off_bias',new_off_bias)
+m.write('row','deselect',new_off_bias) # changed from sq1, for B3
+time.sleep(1)
+
+# reset the row_order to strobe just the desired row
+m.write('ac','row_order',[row]*41)
+time.sleep(0.2)
+
+# re-bias this SQ1 only
+new_on_bias[row] = on_bias[row]
+time.sleep(0.1)
+m.write('ac','on_bias',new_on_bias)
+m.write('row','select',new_on_bias) # changed from sq1, for B3
+time.sleep(0.1)
+m.write('rca', 'flx_lp_init', [1])
+time.sleep(0.1)
+
+# stop muxing for the BAC (or fast-switching BC2) too #commented out for B3
+cv = zeros(n_cols,int)
+#if use_fastSQ2:
+#    for col in range(0,n_cols):
+#        fb2val = m.read('sq2','fb_col%d'%(col))
+#        m.write('sq2','fb_col%d'%(col),[fb2val[row]]*41)
+#        time.sleep(0.01)
+#m.write('sq2','enbl_mux',[0])
+time.sleep(0.01)
+
+# SET ALL ADC OFFSETS TO BE THE SAME
+# (avoids bizarre feedback values)
+adc_offsets = zeros(n_cols,int)
+for rc in range(0,n_rc):
+    for col in range(0,MCE_COLS):
+        adc_offsets[rc*MCE_COLS+col] = m.read('rc%d'%(rc+1),'adc_offset%d'%(col))[row]
+new_adc_offsets = tile(adc_offsets,(41,1))
+
+print('adc_offsets =', adc_offsets)
+print('new_adc_offsets =', new_adc_offsets)
+
+print 'setting adc offsets to match desired row'
+for rc in range(0,n_rc):
+    for col in range(0,MCE_COLS):
+        m.write('rc%d'%(rc+1),'adc_offset%d'%col,new_adc_offsets[:,rc*MCE_COLS+col].tolist())
+        time.sleep(0.01)
+time.sleep(0.1)
+m.write('rca', 'flx_lp_init', [1])
+time.sleep(0.1)
+
+print 'setting sq1 bias to match desired row - reading sq1 bias from fast switching values'
+new_sq1_bias = zeros(n_cols,int)
+for col in range(n_cols):
+    new_sq1_bias[col] = m.read('sq1','bias_col%d'%col)[row]
+print 'new sq1 bias: ', new_sq1_bias
+for col in range(n_cols):
+    m.write('sq1','bias_col%d'%col, [new_sq1_bias[col]]*41)
+    time.sleep(0.01)
+time.sleep(0.1)
+
+print 'setting sa fb to match desired row - reading sa fb from fast switching values'
+new_sa_fb = zeros(n_cols,int)
+for col in range(n_cols):
+    new_sa_fb[col] = m.read('sa','fb_col%d'%col)[row]
+print 'new sa fb: ', new_sa_fb
+for col in range(n_cols):
+    m.write('sa','fb_col%d'%col, [new_sa_fb[col]]*41)
+    time.sleep(0.01)
+time.sleep(0.1)
+
+# set the off_bias of this row to match the on_bias (remove SQ1 strobe)
+print 'setting off_bias to match on_bias for this row'
+new_off_bias[row]=on_bias[row]
+m.write('ac','off_bias',new_off_bias)
+m.write('row','deselect',new_off_bias) # changed from sq1, for B3
+time.sleep(0.1)
+m.write('rca', 'flx_lp_init', [1])
+
+time.sleep(0.2)
+
+# disable mux if requested
+# (N.B.: I previously marked this as "BAD - do not do this!"...)
+if opts.sq1_nomux:
+    print 'disabling SQ1 switching entirely'
+    try:
+        m.write('ac','const_val',new_on_bias)
+        m.write('ac','enbl_mux',[0]*41)
+    except ParamError:
+        print 'WARNING: cannot disable SQ1 muxing.  AC.const_val not enabled in mce.cfg'
+    time.sleep(0.1)
+
+# set all Igains to match those for this row
+print 'setting I gains to match desired row'
+Igains = zeros(n_cols,int)
+for rc in range(0,n_rc):
+    for col in range(0,MCE_COLS):
+        Igains[rc*MCE_COLS+col] = m.read('rc%d'%(rc+1),'gaini%d'%(col))[row]
+new_Igains = tile(Igains,(41,1))
+for rc in range(0,n_rc):
+    for col in range(0,MCE_COLS):
+        m.write('rc%d'%(rc+1),'gaini%d'%col,new_Igains[:,rc*MCE_COLS+col].tolist())
+        time.sleep(0.01)
+time.sleep(0.1)
+m.write('rca', 'flx_lp_init', [1])
+time.sleep(0.1)
+
+
+# CHOOSE LOOP CONFIGURATION
+if opts.do_open:
+    # go to open-loop mode
+    print 'going to open-loop mode'
+    # FIND FB1_CONST VALUES TO APPLY
+    # read in a few frames of data in data_mode=1
+    m.write('rca','data_mode',[1])
+    m.write('rca','flx_lp_init',[1])
+    time.sleep(1)
+    if opts.do_direct_acq:
+        # OLD CODE: Read the frames directly into Python
+        print 'reading frames'
+        frames = m.read_frames(opts.n_frames,data_only=True)
+        time.sleep(1)
+        # average all pixels over all frames
+        print 'averaging frames'
+        fb = fix(mean(array(frames),axis=0)/2**12).astype(int)
+        # wrap into 14 bit signed range
+        #fb = fb%2**14
+        #for ii in range(0,len(fb)):
+        #    if fb[ii]>(2**13-1):
+        #        fb[ii] = fb[ii] - 2**14
+    else:
+        # ALTERNATE CODE: Make a file and read it in
+        daqcmd = 'mce_fast_acq %d s 38 %d'%(opts.n_frames, n_rows)
+        print daqcmd
+        os.system(daqcmd)
+        time.sleep(1)
+
+        # what file did I take?
+        datdir = '/data/cryo/current_data/'
+        files = glob(os.path.join(datdir,'fast_??????????'))
+        files.sort(lambda x,y:cmp(os.path.getmtime(x),
+                                  os.path.getmtime(y)))
+        fn = files[-1]
+
+        # read the data in
+        print 'load the new data file'
+        f = SmallMCEFile(fn)
+        d=f.Read()
+        frames = d.data
+        # average over all frames (CHECK THIS!!)
+        # fb = fix(mean(array(frames),axis=1)).astype(int)
+        fb = fix(mean(array(frames),axis=0)/2**12).astype(int)
+    
+    print('fb (raw) = ', fb)
+    # In the future, mce will work directly with numpy arrays...
+    fb = array(fb).reshape((n_rows,-1))
+    # Find integer fb_const values to apply
+    fb_const = fb[row,0:n_cols]
+    print ('fb_const = ', fb_const)
+
+    # FREEZE THE MCE ON THIS ROW WITH THESE FB1's
+    # write the new fb_const vales
+    m.write('sq1','fb_const',fb_const.tolist())
+    # turn servo off
+    m.write('rca','servo_mode',[0]*MCE_COLS)
+    # read out error signal
+    m.write('rca','data_mode',[0])
+    m.write('rca','flx_lp_init',[1])
+    time.sleep(1)
+    print 'frozen in open-loop mode'
+    print '(data_mode = 0)'
+
+    # disable mux => BAD, do not do this!!
+    #m.write('ac','enbl_mux',[0])
+    #m.write('rca','flx_lp_init',[1])
+    #time.sleep(0.2)
+    #print 'MUX off'
+else:
+    # go to closed-loop mode
+    print 'frozen in closed-loop mode'
+    print '(data_mode unchanged)'
