3ab3c15ca7acf30e98e7c3841178df86b07f29a6
jadesand <jadesand@stanford.edu>
2026-02-12 17:21:28 -0800

    fix run_mce_raw_acq execution

--- settling_time/run_mce_raw_acq_two_level.sh
+++ settling_time/run_mce_raw_acq_two_level.sh
@@ -7,5 +7,5 @@ for CS in 10 11 12 13 14 15 16 17; do
     mas_param set row_order 0 1 2 3 4 5 6 7 8 9 ${CS}
     auto_setup --rc=2
     
-    run_mce_raw_acq 1 "${default_columns[@]}" "${default_rcs[@]}"
+    "$(dirname "$0")/run_mce_raw_acq.sh" 1 "${default_columns[@]}" "${default_rcs[@]}"
 done
