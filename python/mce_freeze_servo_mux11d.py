fe6b37504b001728c9d1a08c63555e4ea60157de
jadesand <jadesand@stanford.edu>
2026-03-26 01:28:54 -0700

    freeze_servo

---
+++ python/mce_freeze_servo_mux11d.py
@@ -0,0 +1,220 @@
+#!/usr/bin/env python
+
+import numpy as np
+
+import os
+import time
+
+from mce_control import mce_control
+import auto_setup as ast
+
+USAGE="""
+%prog [options] stage
+
+Configure the MCE for open loop noise measurements.  Basically:
+ - turns off a bunch of biases
+ - disables the servo
+ - disables muxing
+ - for SQ1 measurements, sets a fixed feedback at the current lock point.
+
+The "stage" argument indicates the highest stage you want to leave
+biased.  I.e.:
+
+   preamp - turn off sa biases and sa offsets.
+   sa     - turn off sq2 biases.
+   sq2    - turn off muxing, sq1 biases.
+   sq1    - turn off muxing, tes biases.  Requires a --row argument.
+   tes    - turn off muxing.  Requires a --row argument.
+
+This program is not good at turning things on carefully.  It is mostly
+for turning things off.  So you probably want to run mce_reconfig
+before you run this, or pass "--reconfig" to have the program pre-run
+it for you.
+"""
+
+from optparse import OptionParser
+o = OptionParser(usage=USAGE)
+o.add_option('--config', default=None, type=str,
+             help="tune config file.  If not provided, will use the default config file for the current setup.")
+o.add_option('--row', default=None, type=int,
+             help="specify row to lock (for sq1 and tes stages).")
+o.add_option('--reconfig', action='store_true',
+             help="run mce_reconfig before setting up open loop.")
+o.add_option('--frames', default=30, type=int)
+o.add_option('--no-reinit', action='store_true',
+             help="for tes and sq1 stages, do not re-init the servo before "
+             "measuring the locking feedback.")
+o.add_option('--sq1lock', default='up', type="choice", 
+             choices=('up', 'dn'),
+             help="which SQ1 lock point to use for the locking feedback: 'up' or 'dn'.  Default is 'up'.")
+opts, args = o.parse_args()
+
+STAGES = ['preamp', 'sa', 'sq1', 'tes']
+
+if len(args) != 1 or args[0] not in STAGES:
+    o.error("Provide a single stage argument (%s)." % ','.join(STAGES))
+
+stage = args[0]
+if stage in ['sq1','tes'] and opts.row is None:
+    o.error("The %s stage requires a --row argument." % stage)
+
+# A useful function...
+
+def read_and_zero(mce, card, param):
+    """
+    Read values from card,param.  Set them to zero in the MCE.  Return
+    the read values.
+    """
+    vals = mce.read(card, param)
+    mce.write(card, param, np.zeros(len(vals), 'int'))
+    return vals
+
+# Load config file
+if opts.config is None:
+    mas_path = ast.util.mas_path()
+    exp_file = mas_path.experiment_file()
+else:
+    exp_file = opts.config
+cfg = ast.config.configFile(exp_file)
+tune_path = os.path.dirname(exp_file)
+
+
+# Reconfigure?
+if opts.reconfig:
+    os.system('mce_reconfig')
+
+# Get MCE
+mce = mce_control()
+
+if stage == 'sq1':
+    read_and_zero(mce, 'tes', 'bias')
+
+if stage in ['sq1', 'tes']:
+    # Re-lock
+    if not opts.no_reinit:
+        mce.init_servo()
+        time.sleep(0.1)
+    
+    # Check lock:
+    print 'Columns that appear locked:'
+    mce.data_mode(0)
+    data = mce.read_data(opts.frames, row_col=True).data[opts.row,:,:]
+    err, derr = data.mean(axis=-1), data.std(axis=-1)
+    locked = (abs(err) < derr*2).astype('int')
+    print locked
+
+    # Measure the feedback.
+    if opts.config is not None:
+        print 'Pulling locking SQ1 feedbacks from tune=', tune_path
+        # Load tune
+        fs = ast.util.FileSet(tune_path)
+        tuning = ast.util.tuningData(exp_file=fs.get('cfg_file'), data_dir='')
+        sq = ast.SQ1Ramp.join([ast.SQ1Ramp(f) for f in fs.stage_all('sq1_ramp_check')])
+        sq.tuning = tuning
+        # Compute locking feedbacks from tune (and other useful info)
+        sq.reduce()
+        # Get data shape
+        n_row, n_col, n_fb = sq.data_shape[-3:]
+        # Generate a view of the locking feedbacks by row,col
+        lock_x=sq.analysis['lock_%s_x'%opts.sq1lock].reshape(n_row, n_col)
+        # Print locking feedbacks for desired row
+        fb=lock_x[opts.row].astype('int')
+    else:
+        mce.data_mode(1)
+        data = mce.read_data(opts.frames, row_col=True).extract('fb')[opts.row,:,:]
+        fb, dfb = data.mean(axis=-1), data.std(axis=-1)
+
+
+    print 'Locking feedback:'
+    # There may be a good reason the column is in columns_off ; ie we might
+    # never want voltage down these SQ1FB lines.  Send 0V, for the SQ1FB
+    # is -8192 DAC.
+    #fb[np.where(locked==0)]=-8192 # zero for the sq1 fb lines is -8192
+    columns_off=np.array(cfg['columns_off'][:len(fb)])
+    fb[np.where(columns_off==1)]=-8192
+    print fb.astype('int')
+    # Set fb_const (kill the servo below)
+    mce.fb_const(fb.astype('int'))
+
+    # Fix the row select/deselects first. 
+    ac_select=np.array(mce.read('ac', 'on_bias'))
+    ac_deselect=np.array(mce.read('ac', 'off_bias'))
+    ac_row_order=np.array(mce.read('ac','row_order'))
+    for (ac_idx,(ac_sel,ac_desel)) in enumerate(zip(ac_select,ac_deselect)):
+        if ac_idx==opts.row:
+            ac_deselect[int(ac_row_order[ac_idx])]=ac_select[int(ac_row_order[ac_idx])]
+        else: 
+            ac_select[int(ac_row_order[ac_idx])]=ac_deselect[int(ac_row_order[ac_idx])]
+    
+    mce.write('ac', 'on_bias', ac_select)
+    mce.write('ac', 'off_bias', ac_deselect)
+    # Sleep for a bit to let those biases get written, then disable mux.
+    time.sleep(.1)
+    # Reports from users indicate that setting enbl_mux=0 does not
+    # work... so we set it to 1.
+    ##mce.write('ac', 'enbl_mux', [0])
+    mce.write('ac', 'enbl_mux', [1])
+
+    # If fast-switching the SQ1 bias, set it to match the chosen row.
+    # Also disable the SQ1 muxing.  Already chose SQ1 fb above.
+    sq1_mux = mce.read('sq1', 'enbl_mux')
+    if np.any(sq1_mux):
+        sq1_bias=[]
+        for c in range(len(sq1_mux)):
+            sq1_bias.append(mce.read('sq1', 'bias_col%i'%c)[opts.row])
+        # It is necessary to disable muxing before writing the new
+        # (non fast-switching) SQ1 bias, otherwise it does not get
+        # applied.
+        mce.write('sq1', 'enbl_mux', np.zeros(len(sq1_mux)))
+        time.sleep(.1)
+        mce.write('sq1', 'bias', sq1_bias)
+
+    # If fast-switching the SA fb, set it to match the chosen row.
+    # Also disable the SA fb muxing.
+    sa_mux = mce.read('sa', 'enbl_mux')
+    if np.any(sa_mux):
+        sa_fb=[]
+        for c in range(len(sa_mux)):
+            sa_fb.append(mce.read('sa', 'fb_col%i'%c)[opts.row])
+        # It is necessary to disable muxing before writing the new
+        # (non fast-switching) SA fb, otherwise it does not get
+        # applied.
+        mce.write('sa', 'enbl_mux', np.zeros(len(sa_mux)))
+        time.sleep(.1)
+        mce.write('sa', 'fb', sa_fb)
+
+if stage == 'sa':
+    # Kill the SQ1 bias and disable mux.
+    read_and_zero(mce, 'ac', 'on_bias')
+    read_and_zero(mce, 'ac', 'off_bias')
+    time.sleep(.1)
+    mce.write('ac', 'enbl_mux', [0])
+    # Also disable SQ1 muxing.  Hope you chose a good sq1 fb.
+    sq1_mux = mce.read('sq1', 'enbl_mux')
+    if np.any(sq1_mux):
+        mce.write('sq1', 'enbl_mux', np.zeros(len(sq1_mux)))
+    time.sleep(.1)
+
+    # Kill the SQ1 bias
+    read_and_zero(mce, 'sq1', 'bias')
+
+    # Disable SA fb muxing
+    sa_mux = mce.read('sa', 'enbl_mux')
+    if np.any(sa_mux):
+        mce.write('sa','enbl_mux', np.zeros(len(sa_mux)))
+    time.sleep(.1)
+    
+    # It is necessary to disable muxing before writing the new
+    # (non fast-switching) SA feedback, otherwise it does not get
+    # applied.
+    sa_fb = mce.read('sa', 'fb')
+    mce.write('sa','fb',sa_fb)
+
+if stage == 'preamp':
+    read_and_zero(mce, 'sa', 'bias')
+    read_and_zero(mce, 'sa', 'offset')
+
+# You will probably want error mode data, with the servo off.
+mce.servo_mode(1)
+mce.data_mode(0)
+
