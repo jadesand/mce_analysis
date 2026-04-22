aee747bfe354307f2dcf2ccb7730270bfe1db46d
jadesand <jadesand@stanford.edu>
2026-04-22 00:41:22 -0700

    update

--- script/take_250_two_level.sh
+++ script/take_250_two_level.sh
@@ -9,14 +9,19 @@ gain=-30
 
 mas_param set sq1_ramp_tes_bias 0
 
-python /home/mce/rshi/mce_scripts/python/go_250kHz_two_level.py ${col} ${row} `date +%`
 
-mce_cmd -x wb rc2 gaini${col_rc} ${gain} ${gain} ${gain} ${gain} ${gain} ${gain} ${gain} ${gain} ${gain} ${gain} ${gain}
+cd ~/rshi/mce_scripts/python
+
+python go_250kHz_two_level.py ${col} ${row} `date +%`
+
+mce_cmd -x wb rc2 gaini${col_rc} ${gain} ${gain} ${gain} ${gain} ${gain} ${gain} ${gain} ${gain} ${gain} ${gain} ${gain} ${gain} ${gain} ${gain} ${gain} ${gain} ${gain} ${gain} ${gain} ${gain} ${gain} ${gain} ${gain} ${gain} ${gain} ${gain} ${gain} ${gain} ${gain} ${gain} ${gain} ${gain} ${gain} ${gain} ${gain} ${gain} ${gain} ${gain} ${gain} ${gain} ${gain}
 
 mce_run `date +%s`_nr2_dr256_dm4_250kHz_r${row}c${col} 25000 2
 
-for rc in 2; do for c in `seq 0 7`; do mce_cmd -x wb rc${rc} gaini${c} 0 0 0 0 0 0 0 0 0 0 0; done; done
+for rc in 2; do for c in `seq 0 7`; do mce_cmd -x wb rc${rc} gaini${c} 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0; done; done
 for rc in 2; do mce_cmd -x wb rc${rc} data_mode 0; done
+for rc in 2; do mce_cmd -x wb rc${rc} fb_const 440; done
+mce_cmd -x wb sq1 fb_const 440
 mce_run `date +%s`_nr2_dr256_dm0_250kHz_r${row}c${col}_fboff 25000 2
 
 # mce_cmd -x wb rca flx_lp_init 1
