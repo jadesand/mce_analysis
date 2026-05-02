acef6822b3a5389745be17a73d8ea2cc1af36046
jadesand <jadesand@stanford.edu>
2026-05-01 21:58:16 -0700

    add date folder

--- script/reconfig_two_level.sh
+++ script/reconfig_two_level.sh
@@ -3,10 +3,11 @@ SCRIPT_NAME=$(basename "$0")
 
 tune_ctime=""
 cs="0"
+date="current_data"
 
 # Parse keyword arguments
-opts=$(getopt -o t:c: \
-    --long tune-ctime:,cs: \
+opts=$(getopt -o t:c:d: \
+    --long tune-ctime:,cs:,date: \
     -n "$SCRIPT_NAME" -- "$@")
 
 if [ $? -ne 0 ]; then echo "Error parsing options"; exit 1; fi
@@ -16,6 +17,7 @@ while true; do
     case "$1" in
         -t|--tune-ctime)    tune_ctime="$2"; shift 2 ;;
         -c|--cs)            cs="$2"; shift 2 ;;
+        -d|--date)          date="$2"; shift 2 ;;
         --)                 shift; break ;;
         *)                  echo "Unknown option: $1"; exit 1 ;;
     esac
@@ -23,5 +25,5 @@ done
 
 mce_zero_bias > /dev/null
 sleep 1
-mce_make_config -x -e $MAS_DATA/two_level_${tune_ctime}/CS${cs}/experiment.cfg 
+mce_make_config -x -e $MAS_DATA_ROOT/${date}/two_level_${tune_ctime}/CS${cs}/experiment.cfg 
 sleep 1
