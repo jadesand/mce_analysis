5774c7f3d8c6913223723b4b64284d77db1c4f77
jadesand <jadesand@stanford.edu>
2026-05-01 21:58:08 -0700

    add keyword arguments

--- script/run_mce_raw_acq_two_level.sh
+++ script/run_mce_raw_acq_two_level.sh
@@ -1,12 +1,39 @@
 #!/bin/bash
 
-if [[ -z "$1" ]]; then
-    echo "Usage: $(basename "$0") <ctime>"
-    echo "Error: tune ctime is required"
-    exit 1
-fi
+SCRIPT_NAME=$(basename "$0")
+FREEZE_SCRIPT="/home/mce/rshi/mce_scripts/python/mce_freeze_servo_mux11d.py"
 
-ctime=$1
+tune_ctime=""
+date="current_data"
+columns=(4 5 6 7)
+chips=(10 11 12 13 14 15 16 17)
+rcs=(2)
+freeze_stage=""
+row=0
+ndatasets=50
+
+
+# Parse keyword arguments
+opts=$(getopt -o t:R:c:C:d:f:r:n: \
+    --long tune-ctime:,rcs:,col:,chip:,date:,freeze-stage:,row:,ndatasets: \
+    -n "$SCRIPT_NAME" -- "$@")
+if [ $? -ne 0 ]; then echo "Error parsing options"; exit 1; fi
+eval set -- "$opts"
+
+while true; do
+    case "$1" in
+        -t|--tune-ctime)    tune_ctime="$2"; shift 2 ;;
+        -R|--rcs)           IFS=',' read -r -a rcs     <<< "$2"; shift 2 ;;
+        -c|--col)           IFS=',' read -r -a columns <<< "$2"; shift 2 ;;
+        -C|--chip)          IFS=',' read -r -a chips   <<< "$2"; shift 2 ;;
+        -d|--date)          date="$2"; shift 2 ;;
+        -f|--freeze-stage)  freeze_stage="$2"; shift 2 ;;
+        -r|--row)           row="$2"; shift 2 ;;
+        -n|--ndatasets)     ndatasets="$2"; shift 2 ;;
+        --)                 shift; break ;;
+        *)                  echo "Unknown option: $1"; exit 1 ;;
+    esac
+done
 
 MAS_DATA_REAL=$(readlink -f "$MAS_DATA")
 
@@ -14,21 +41,29 @@ PARENT_NAME="two_level_raw_$(date +%s)"
 mkdir -p "$MAS_DATA_REAL/$PARENT_NAME"
 mkdir -p "$MAS_DATA_REAL/analysis/$PARENT_NAME"
 
-columns=(4 5 6 7)
 columns_str=$(IFS=,; echo "${columns[*]}")
-
-rcs=(2)
 rcs_str=$(IFS=,; echo "${rcs[*]}")
 
 {
-for CS in 10 11 12 13 14 15 16 17; do
-# for CS in 10; do
-    mce_zero_bias > /dev/null 2>&1
-    sleep 1
-    mce_make_config -x -e "$MAS_DATA_REAL/two_level_${ctime}/CS$((CS-10))/experiment.cfg"
-    sleep 1
-    
-    "$(dirname "$0")/run_mce_raw_acq.sh" 1 "$columns_str" "$rcs_str"
+for CS in "${chips[@]}"; do
+    if [[ -n "$freeze_stage" ]]; then
+        echo "Freezing servo for CS=$CS, row=$row, stage=$freeze_stage..."
+        mce_zero_bias > /dev/null
+        sleep 1
+        cp "$MAS_DATA_ROOT/${date}/two_level_${tune_ctime}/CS$((CS-10))/experiment.cfg" $MAS_DATA/experiment.cfg
+        # Be careful here
+        if [[ "$freeze_stage" == "preamp" ]] || [[ "$freeze_stage" == "sa" ]]; then
+            auto_setup --rc=2 --last-stage=sa_ramp
+        else
+            auto_setup --rc=2 --last-stage=sq1_ramp
+        fi
+        sleep 1
+        python "$FREEZE_SCRIPT" --row "$row" $freeze_stage
+    else
+        echo "No freeze stage specified, reconfig CS=$CS."
+        reconfig_two_level -t "$tune_ctime" -c "$CS" -d "$date"
+    fi
+    "$(dirname "$0")/run_mce_raw_acq.sh" "$ndatasets" "$columns_str" "$rcs_str"
 
     # Find the most recently created directory
     LATEST=$(find "$MAS_DATA_REAL" -maxdepth 1 -type d \
